db85f263c35f9edf19bb2ed6bb519570afcb5361
parkinhwan-web <inhwanbag26@gmail.com>
2025-03-21 18:05:28 +0900

    수정 7

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/")
+    @GetMapping(value = "/", produces = "text/plain;charset=UTF-8")
     public String hello() {
         return "서버가 정상적으로 작동 중입니다!";
     }
