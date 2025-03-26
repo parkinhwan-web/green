a25c092c42d8e04ad1cf3ca368cfb4a85c769f47
parkinhwan-web <inhwanbag26@gmail.com>
2025-03-26 14:44:41 +0900

    수정 20

--- src/main/java/com/example/demo/dto/RecycleLogRequest.java
+++ src/main/java/com/example/demo/dto/RecycleLogRequest.java
@@ -1,8 +1,12 @@
 package com.example.demo.dto;
 
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+@NoArgsConstructor          // ✅ 추가
+@AllArgsConstructor
 @Getter
 @Setter
 public class RecycleLogRequest {
