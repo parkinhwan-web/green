4cdee7b83a69d980889f61fa1684b4924bafc2ab
parkinhwan-web <inhwanbag26@gmail.com>
2025-05-25 04:56:15 +0900

    수정42

--- src/main/java/com/example/demo/dto/UserLoginResponse.java
+++ src/main/java/com/example/demo/dto/UserLoginResponse.java
@@ -6,14 +6,25 @@ import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
 @Getter
-@Builder
 @NoArgsConstructor
-@AllArgsConstructor
 public class UserLoginResponse {
+
     private String message;
     private String email;
     private String username;
     private Long userId;
     private String token;
     private int expiresIn;
+
+    @Builder
+    public UserLoginResponse(String message, String email, String username,
+                             Long userId, String token, int expiresIn) {
+        this.message = message;
+        this.email = email;
+        this.username = username;
+        this.userId = userId;
+        this.token = token;
+        this.expiresIn = expiresIn;
+    }
 }
+
