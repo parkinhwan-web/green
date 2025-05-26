9710448996b02b42b07ee9dfafd7b14a14d2c00a
parkinhwan-web <inhwanbag26@gmail.com>
2025-05-26 20:01:42 +0900

    수정 67 쿠폰 목록록

--- src/main/java/com/example/demo/entity/Coupon.java
+++ src/main/java/com/example/demo/entity/Coupon.java
@@ -2,24 +2,41 @@ package com.example.demo.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "coupons")
 @Getter
 @Setter
+@NoArgsConstructor
 public class Coupon {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String brandName;
+
+    @Column(nullable = false)
     private String productName;
-    private int points;
+
+    @Column(nullable = false)
+    private Integer points;
+
+    @Column(nullable = false)
     private String category;
+
+    @Column(nullable = false)
     private String imageUrl;
-    private int expireDays;
-    private boolean available;
+
+    @Column(nullable = false)
+    private Integer expireDays;
+
+    @Column(nullable = false, length = 5)  // "true" 또는 "false" 문자열 저장
+    private String available;
+
+    @Column(nullable = false)
     private String description;
 }
