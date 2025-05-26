14598d3c277697c2f770e4334605b8a706b8b65c
parkinhwan-web <inhwanbag26@gmail.com>
2025-05-26 20:59:00 +0900

    수정 72

--- src/main/java/com/example/demo/repository/CouponRepository.java
+++ src/main/java/com/example/demo/repository/CouponRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
-    List<Coupon> findByAvailableTrue(); // ✅ 파라미터 없이 선언
+    List<Coupon> findByAvailable(boolean available); // ✅ 파라미터 없이 선언
 
 
 }
